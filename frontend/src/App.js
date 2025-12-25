b44a7761b0fccaf79cb30c72a1a4294942ab67be
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-25 18:39:02 +0530

    NAVIGATION COMPLETED

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
-    <div >
-     
-    </div>
+    < >
+     <Navigation />
+    </>
   );
 }
 
